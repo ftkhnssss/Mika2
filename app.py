1692fbc7ef474a7cd507bfe397447bbda53b4f1a
ftkhnssss <145285585+ftkhnssss@users.noreply.github.com>
2024-06-03 03:05:47 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,77 +1,9 @@
+import streamlit as st
 import time
 import uuid
-import google.generativeai as genai
-import streamlit as st
-import firebase_admin
-from firebase_admin import credentials, db
-from firebase_admin.exceptions import FirebaseError
-from config import GEMINI_API_KEY
+import json
 
-# Initialize Firebase only if not already initialized
-if not firebase_admin._apps:
-    try:
-        # Inisialisasi Firebase menggunakan kredensial dari serviceAccountKey.json
-        cred = credentials.Certificate("mika-test-f7138-firebase-adminsdk-qnp9p-ac39df705b.json")
-        firebase_admin.initialize_app(cred, {
-            'databaseURL': 'https://mika-test-f7138-default-rtdb.firebaseio.com/'
-        })
-    except (FileNotFoundError, FirebaseError) as e:
-        st.error(f"Failed to initialize Firebase: {e}")
-        st.stop()
-
-# Konfigurasi Kunci API
-genai.configure(api_key=GEMINI_API_KEY)
-
-# Fungsi untuk memulai sesi obrolan
-@st.cache(allow_output_mutation=True)
-def start_chat():
-    # Konfigurasi untuk pembangkitan teks dan pengaturan keamanan
-    generation_config = {
-        "temperature": 1,
-        "top_p": 0.95,
-        "top_k": 64,
-        "max_output_tokens": 8192,
-        "response_mime_type": "text/plain",
-    }
-    safety_settings = [
-        {"category": "HARM_CATEGORY_HARASSMENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_HATE_SPEECH", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_SEXUALLY_EXPLICIT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-        {"category": "HARM_CATEGORY_DANGEROUS_CONTENT", "threshold": "BLOCK_MEDIUM_AND_ABOVE"},
-    ]
-    # Inisialisasi model
-    model = genai.GenerativeModel(
-        model_name="gemini-1.5-flash",
-        safety_settings=safety_settings,
-        generation_config=generation_config,
-        system_instruction="Hi! Saya Mika, asisten kesehatan virtual Anda. Silakan perkenalkan diri Anda di awal percakapan. Saya bisa membantu Anda dengan berbagai pertanyaan kesehatan. Pastikan untuk memberikan informasi lengkap dan detail agar saya bisa memberikan saran yang akurat. 😊"
-    )
-    return model.start_chat(history=[])
-
-# Fungsi untuk menyimpan percakapan ke dalam Firebase
-def save_chat_history_to_firebase(chat_history, session_id):
-    ref = db.reference(f'chat_history/{session_id}')
-    ref.set(chat_history)
-
-# Fungsi untuk memuat percakapan dari Firebase
-def load_chat_history_from_firebase(session_id):
-    ref = db.reference(f'chat_history/{session_id}')
-    chat_history = ref.get()
-    return chat_history if chat_history else []
-
-# Fungsi untuk menampilkan pesan asisten dengan animasi mengetik
-def type_message(message, placeholder):
-    typed_text = ""
-    for char in message:
-        typed_text += char
-        placeholder.markdown(f"""
-            <div style="display: flex; justify-content: flex-start; margin-bottom: 10px; color: black;">
-                <div style="background-color: #FFFFFF; padding: 10px; border-radius: 10px; max-width: 85%; border: 1px solid #ccc;">
-                    {typed_text}
-                </div>
-            </div>
-            """, unsafe_allow_html=True)
-        time.sleep(0.05)  # Adjust the typing speed here
+from percakapan import start_chat, save_chat_history, load_chat_history, type_message
 
 # Program utama
 def main():
@@ -88,7 +20,7 @@ def main():
 
     # Inisialisasi riwayat obrolan jika belum ada
     if 'chat_history' not in st.session_state:
-        st.session_state.chat_history = load_chat_history_from_firebase(session_id)
+        st.session_state.chat_history = load_chat_history(session_id)
 
     # Input pengguna
     user_input = st.text_input("Ketik pesan Anda di sini:")
@@ -125,8 +57,8 @@ def main():
             st.session_state.chat_history.append(("Anda", user_input))
             st.session_state.chat_history.append(("Mika", response.text))
             
-            # Simpan riwayat obrolan ke Firebase
-            save_chat_history_to_firebase(st.session_state.chat_history, session_id)
+            # Simpan riwayat obrolan ke dalam file JSON
+            save_chat_history(st.session_state.chat_history, session_id)
             
             # Menghapus placeholder dan menampilkan pesan bot yang sebenarnya per huruf
             typing_placeholder.empty()
@@ -149,7 +81,7 @@ def main():
                 """, unsafe_allow_html=True)
         else:
             st.markdown(f"""
-                <div style="display: flex; justify-content: flex-start; margin-bottom: 10px; color: black;">
+                <div style="display: flex; justify-content: flex-start; margin-bottom: 10px;">
                     <div style="background-color: #FFFFFF; padding: 10px; border-radius: 10px; max-width: 85%; border: 1px solid #ccc; color: black;">
                         {message}
                     </div>
@@ -158,13 +90,9 @@ def main():
 
     # Tombol untuk menghapus riwayat obrolan
     if st.button("Hapus Riwayat Obrolan"):
-        # Hapus riwayat obrolan dari session_state
         st.session_state.chat_history = []
-        # Simpan perubahan ke Firebase
-        save_chat_history_to_firebase(st.session_state.chat_history, session_id)
-        # Mulai kembali sesi obrolan
-        st.session_state.chat_session = start_chat()
-        # Jalankan kembali aplikasi
+        save_chat_history(st.session_state.chat_history, session_id)  # Simpan perubahan ke dalam file JSON
+        st.session_state.chat_session = start_chat()  # Memulai kembali sesi obrolan
         st.experimental_rerun()
 
 if __name__ == "__main__":
